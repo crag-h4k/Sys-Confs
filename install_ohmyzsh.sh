86b9708e53f6bddb71694a93a5452046395019e6
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2018-05-01 08:13:55 -0600

    Update install_ohmyzsh.sh

--- install_ohmyzsh.sh
+++ install_ohmyzsh.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo ln -s ~/.oh-my-zsh /root/.oh-my-zsh
+sudo ln ~/.zshrc /root/.zshrc
