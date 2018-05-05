8b63ce32dc029e80aea048f6ebcc037cb48a939d
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2018-05-04 20:00:41 -0600

    Update deb_packages.sh

--- deb_packages.sh
+++ deb_packages.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/sh
 
-packages="sudo build-essential make gcc g++ "
+packages="sudo build-essential cmake make gcc g++  "
 packages+="python3 python3-dev python3-pip libssl-dev libffi-dev "
 packages+="vim git zsh tmux"
-pacakages +="net-tools curl "
+pacakages+="net-tools curl "
+
+bro_depen+="python-dev swig zlib1g-dev libgeoIP-dev flex bison libpcap-dev"
+
 
 apt update -y
 apt dist-upgrade -y
 apt install -y netselect-apt
 netselect-apt
 apt install -y $packages
+apt install -y $bro_depen
