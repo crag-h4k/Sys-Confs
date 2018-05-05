5016ba8a351abc06236bdb8554daf66eb74e1f09
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2018-05-05 12:07:21 -0600

    Update deb_packages.sh

--- deb_packages.sh
+++ deb_packages.sh
@@ -5,12 +5,8 @@ packages+="python3 python3-dev python3-pip libssl-dev libffi-dev "
 packages+="vim git zsh tmux"
 pacakages+="net-tools curl htop"
 
-bro_depen+="python-dev swig zlib1g-dev libgeoip-dev flex bison libpcap-dev"
-
-
 apt update -y
 apt dist-upgrade -y
 apt install -y netselect-apt
 netselect-apt
 apt install -y $packages
-apt install -y $bro_depen
