484969ee6b46fbceaeef27fe35ff0f89d5f4046f
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2018-05-05 12:51:36 -0600

    Update deb_packages.sh

--- deb_packages.sh
+++ deb_packages.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 
-packages="sudo build-essential cmake make gcc g++  "
+packages="sudo build-essential cmake make gcc g++ "
 packages+="python3 python3-dev python3-pip libssl-dev libffi-dev "
 packages+="vim git zsh tmux "
-pacakages+="net-tools curl htop "
+pacakages+="net-tools curl htop mailtools "
 
 apt update -y
 apt dist-upgrade -y
