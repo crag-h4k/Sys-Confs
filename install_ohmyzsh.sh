b5d8556c950bc77de472193b9076eba5f5e10eec
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2018-05-05 12:05:38 -0600

    Update install_ohmyzsh.sh

--- install_ohmyzsh.sh
+++ install_ohmyzsh.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/bash
+#zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo ln -s ~/.oh-my-zsh /root/.oh-my-zsh
+cp ~/Sys-Configurations/.zshrc ~/.zshrc
 sudo ln ~/.zshrc /root/.zshrc
+source ~/.zshrc
+sudo su
+sh -c $(which zsh)
+source /root/.zshrc
+
+#vim
+cp ~/Sys-Configurations/.vimrc ~/.vimrc
+cp -r ~/Sys-Configurations/vim ~/.vim
+sudo ln -s ~/.vim /root/.vim
+sudo ln ~/.vimrc /root/.vimrc
