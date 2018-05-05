2351e54af4cbce7f0da035eece57a9f1a3710715
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2018-05-05 12:36:34 -0600

    Update configure_zsh_vim.sh

--- configure_zsh_vim.sh
+++ configure_zsh_vim.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 #zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo ln -s ~/.oh-my-zsh /root/.oh-my-zsh
-cp ~/Sys-Configurations/.zshrc ~/.zshrc
-sudo ln ~/.zshrc /root/.zshrc
-source ~/.zshrc
-sudo su
+ln -s /home/$SUDO_USER/.oh-my-zsh /root/.oh-my-zsh
+cp /home/$SUDO_USER/Sys-Configurations/.zshrc /home/$SUDO_USER/.zshrc
+sudo ln /home/$SUDO_USER/.zshrc /root/.zshrc
+source /home/$SUDO_USER/.zshrc
+su root
 sh -c $(which zsh)
 source /root/.zshrc
 
 #vim
-cp ~/Sys-Configurations/.vimrc ~/.vimrc
-cp -r ~/Sys-Configurations/vim ~/.vim
-sudo ln -s ~/.vim /root/.vim
-sudo ln ~/.vimrc /root/.vimrc
+cp /home/$SUDO_USER/Sys-Configurations/.vimrc /home/$SUDO_USER/.vimrc
+cp -r /home/$SUDO_USER/Sys-Configurations/vim /home/$SUDO_USER/.vim
+ln -s /home/$SUDO_USER/.vim /root/.vim
+ln /home/$SUDO_USER/.vimrc /root/.vimrc
