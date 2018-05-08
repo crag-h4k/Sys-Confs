79aaf4a7c460381e888149b0aa7c1a7f601bb90c
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2018-05-08 07:20:41 -0600

    Update install_ELK.sh

--- install_ELK.sh
+++ install_ELK.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/bash
 
+apt install apt-transport-https default-jre-headless
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-6.x.list
+
 apt update
-apt install 
+apt install elasticsearch logstash kibana filebeat
+
+echo "Remember to enable these services via command"
+encho "systemctl enable elasticsearch"
