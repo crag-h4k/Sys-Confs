18ff3fca9f6e222026594f22fc6d0966bbad3bd2
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2018-05-12 13:46:34 -0600

    i3, rxvt

--- linux/deb_packages.sh
+++ linux/deb_packages.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/sh
 
-packages=" sudo build-essential cmake make gcc g++ "
-packages+=" python3 python3-dev python3-pip libssl-dev libffi-dev "
+packages=" sudo build-essential cmake make gcc g++ python3 python3-dev python3-pip "
+packages+=" libssl-dev libffi-dev "
 packages+=" vim git zsh tmux "
-pacakages+=" net-tools wget curl htop mailtools "
+packages+=" net-tools wget curl htop mailtools "
+wm_packages+=" i3 suckless-tools rxvt-unicode-256color fonts-hack firefox "
 
 apt update -y
 apt dist-upgrade -y
 apt install -y netselect-apt
 netselect-apt
 apt install -y $packages
+#apt install -y $wm_packages
