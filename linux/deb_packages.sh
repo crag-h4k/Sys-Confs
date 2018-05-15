7afea1da118a453744e3c9589270db2222cb6c8e
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2018-05-15 06:46:32 -0600

    Update deb_packages.sh

--- linux/deb_packages.sh
+++ linux/deb_packages.sh
@@ -4,11 +4,12 @@ packages=" sudo build-essential cmake make gcc g++ python3 python3-dev python3-p
 packages+=" libssl-dev libffi-dev "
 packages+=" vim git zsh tmux "
 packages+=" net-tools wget curl htop mailtools "
-wm_packages+=" i3 suckless-tools rxvt-unicode-256color fonts-hack firefox "
-
+wm_packages=" i3 suckless-tools rxvt-unicode-256color fonts-hack-otf fonts-hack-ttf fonts-hack-web "
+de_packages=" firefox-esr xfce4 "
 apt update -y
 apt dist-upgrade -y
 apt install -y netselect-apt
 netselect-apt
 apt install -y $packages
 #apt install -y $wm_packages
+#apt install -y $de_packages
