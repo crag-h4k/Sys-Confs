9d323912b0024a402539fc100dbd158ff0ccfb04
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2018-08-17 17:37:37 -0600

    Update pi_packages.sh

--- linux/pi_packages.sh
+++ linux/pi_packages.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/sh
 
+packages=" sudo build-essential cmake make gcc g++ python3 python3-dev python3-pip "
+packages+=" libssl-dev libffi-dev "
+packages+=" vim git zsh "
+packages+=" net-tools wget curl htop "
+packages += " wireshark nmap "
+apt update -y
+apt dist-upgrade -y
+apt install -y netselect-apt
+netselect-apt
+apt install -y $packages
+apt update
