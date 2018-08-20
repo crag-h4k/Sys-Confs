1abf2937e64ccf30cf5fbb9ba79f96def54b90bc
deadlift1226 <danemorgan91@gmail.com>
2018-08-20 05:10:09 -0600

    fixed vim stuff

--- linux/deb_packages.sh
+++ linux/deb_packages.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 
-packages=" sudo build-essential cmake make gcc g++ python3 python3-dev python3-pip "
+packages=" sudo build-essential cmake make gcc g++ "
 packages+=" libssl-dev libffi-dev "
-packages+=" vim git zsh tmux "
+packages+=" vim git zsh tmux ncdu "
 packages+=" net-tools wget curl htop mailtools "
 #wm_packages=" i3 suckless-tools rxvt-unicode-256color fonts-hack-otf fonts-hack-ttf fonts-hack-web "
 #de_packages=" firefox-esr xfce4 "
@@ -11,5 +11,8 @@ apt dist-upgrade -y
 apt install -y netselect-apt
 netselect-apt
 apt install -y $packages
+su $SUDO_USER
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh #python3.6
+bash Miniconda3-latest-Linux-x86_64.sh 
 #apt install -y $wm_packages
 #apt install -y $de_packages
