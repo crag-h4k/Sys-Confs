552f537815d350242452344c4edf7f699a294084
deadlift1226 <danemorgan91@gmail.com>
2018-09-12 07:04:27 -0600

    fixed vm stuff

--- linux/deb_packages.sh
+++ linux/deb_packages.sh
@@ -3,15 +3,14 @@
 packages=" sudo build-essential cmake make gcc g++ "
 packages+=" libssl-dev libffi-dev "
 packages+=" vim git zsh tmux ncdu "
-packages+=" net-tools wget curl htop mailtools byobu "
-wm_packages=" i3 suckless-tools rxvt-unicode-256color fonts-hack-otf fonts-hack-ttf fonts-hack-web chromium chromedriver "
-de_packages=" firefox-esr xfce4 "
+packages+=" net-tools wget curl htop mailtools byobu dtrx "
+wm_packages=" conky i3 suckless-tools rxvt-unicode-256color fonts-hack-otf fonts-hack-ttf fonts-hack-web chromium "
+vm_packages=" open-vm-tools open-vm-tools-desktop "
 apt update -y
 apt dist-upgrade -y
 apt install -y netselect-apt
 netselect-apt
 apt install -y $packages
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh #python3.6
-bash Miniconda3-latest-Linux-x86_64.sh 
-#apt install -y $wm_packages
-#apt install -y $de_packages
+#wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh #python3.6
+#bash Miniconda3-latest-Linux-x86_64.sh 
+apt install -y $wm_packages
