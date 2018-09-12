2b0a6cc51e0c77d20c873c447a642fc82c1c6595
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2018-09-12 05:43:14 -0600

    Update deb_packages.sh

--- linux/deb_packages.sh
+++ linux/deb_packages.sh
@@ -3,15 +3,14 @@
 packages=" sudo build-essential cmake make gcc g++ "
 packages+=" libssl-dev libffi-dev "
 packages+=" vim git zsh tmux ncdu "
-packages+=" net-tools wget curl htop mailtools "
-#wm_packages=" i3 suckless-tools rxvt-unicode-256color fonts-hack-otf fonts-hack-ttf fonts-hack-web "
-#de_packages=" firefox-esr xfce4 "
+packages+=" net-tools wget curl htop mailtools byobu "
+wm_packages=" i3 suckless-tools rxvt-unicode-256color fonts-hack-otf fonts-hack-ttf fonts-hack-web chromium chromedriver "
+de_packages=" firefox-esr xfce4 "
 apt update -y
 apt dist-upgrade -y
 apt install -y netselect-apt
 netselect-apt
 apt install -y $packages
-su $SUDO_USER
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh #python3.6
 bash Miniconda3-latest-Linux-x86_64.sh 
 #apt install -y $wm_packages
